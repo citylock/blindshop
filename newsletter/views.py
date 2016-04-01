8a5c1e69f1ee2dc2005024e63a009607cd621f18
citylock <citylock77@gmail.com>
2016-04-01 12:28:25 -0700

    View & Template Context

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    title = "My Title"
+    title = "My Title" 
+    if request.user.is_authenticated():
+        title = "My Title %s" %(request.user)
+
+    # add a form 
     context = {
-        "template_title": title,
+        "title": title,
     }
     return  render(request,"home.html", context)
 
