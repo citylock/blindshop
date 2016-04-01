f15f3102bb7ce166be64504c79bc51850d1b72f4
citylock <citylock77@gmail.com>
2016-04-01 13:18:34 -0700

    Form in a View and data validation

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
-    title = "My Title" 
-    if request.user.is_authenticated():
-        title = "My Title %s" %(request.user)
+    title = "Welcome" 
+    form =  SignUpForm(request.POST or None)
+    # if request.user.is_authenticated():
+    #    title = "My Title %s" %(request.user)
 
-    # add a form 
+    # add a form
+    # print request
+    #if request.method == "POST":
+    #    print request.POST
+
     context = {
         "title": title,
+        "form": form,
     }
+
+    if form.is_valid():
+        #form.save()
+        print request.POST['email']    # not recommend because is not validated, it is raw data
+        instance = form.save(commit=False)
+
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name: 
+            full_name = "New full_name"
+        instance.full_name = full_name
+
+        # if not instance.full_name: 
+        #    instance.full_name = "Justin"
+        instance.save()
+        context = {
+                "title": "Thank you", 
+        }
+
+
     return  render(request,"home.html", context)
 
