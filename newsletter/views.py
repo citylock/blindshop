57e0693282faecaa52652759278e8b1f83bb9845
citylock <citylock77@gmail.com>
2016-03-31 23:51:17 -0700

    views

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return  render(request,"home.html", {})
+    title = "My Title"
+    context = {
+        "template_title": title,
+    }
+    return  render(request,"home.html", context)
 
