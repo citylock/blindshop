4c351ac50aeb26b46187a383be2b9316cda503bb
citylock <citylock77@gmail.com>
2016-04-07 18:58:21 -0700

    26. Styling MVP Landign Part2

--- newsletter/views.py
+++ newsletter/views.py
@@ -43,6 +43,8 @@ def home(request):
 
 
 def contact(request):
+    title = 'Contact Us'
+    title_align_center = True
     form =  ContactForm(request.POST or None)
     if form.is_valid():
         # for key, value in form.cleaned_data.iteritems():
@@ -74,6 +76,8 @@ def contact(request):
 
     context = {
             "form": form,
+            "title": title,
+            "title_align_center": title_align_center,
     }
     return render(request, "forms.html", context)
 
