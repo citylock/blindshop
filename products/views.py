3fd5e3bedd1267865d1f4ad907e7f732fe81a8be
citylock <citylock77@gmail.com>
2016-04-21 09:37:27 -0700

    ecommerce 09.Add HTML Template

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -7,10 +8,26 @@ from .models import Product
 
 class ProductDetailView(DetailView):
 	model = Product 
+	# templatename = "product.html"
+	# template_name = "<appname>/<modelname>_detail.html"
 
 
 def product_detail_view_func(request,id):
-	product_instance = Product.objects.get(id=id)
+	# Method 1
+	# product_instance = Product.objects.get(id=id)
+
+	# Method 2
+	product_instance = get_object_or_404(Product, id=id)
+
+	# Method 3
+	try:
+		product_instance = Product.object.get(id=id)
+	except Product.DoesNotExist: 
+		raise Http404
+	except: 
+		raise Http404
+
+
 	template = "products/product_detail.html"
 	context = {
 		"object": product_instance
