fbf27dad0a98c61965ee9f6a5244db83600e9530
citylock <citylock77@gmail.com>
2016-04-24 08:32:08 -0700

    ecommerce 14.Post Save Signals for Variations

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -36,6 +37,44 @@ class Product(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("product_detail", kwargs={"pk": self.pk})
+
+
+
+class Variation(models.Model):
+	product = models.ForeignKey(Product)
+	title = models.CharField(max_length=120)
+	price = models.DecimalField(decimal_places=2, max_digits=20)
+	sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+	active = models.BooleanField(default=True)
+	inventory = models.IntegerField(null=True, blank=True)  # refer none == unlimited amount
+
+	def __unicode__(self):
+		return self.title
+
+	def get_price(self):
+		if self.sale_price is not None:
+			return self.sale_price
+		else: 
+			return self.price
+
+	def get_absolute_url(self):
+		return self.product.get_absolute_url()
+
+
+def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
+	print sender
+	product = instance
+	variations = product.variation_set.all()
+	if variations.count() == 0:
+		new_var = Variation()
+		new_var.product = product
+		new_var.title = "Default"
+		new_var.price = product.price
+		new_var.save()
+
+post_save.connect(product_post_saved_receiver, sender=Product)
+
+
 
 # Product Image
 
