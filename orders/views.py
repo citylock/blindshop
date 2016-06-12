ac955e5f4f3caa1fee437f62293ab66f135e28a0
citylock <citylock77@gmail.com>
2016-06-12 09:16:08 -0700

    ecommerce2 59.Model Choice Field

--- orders/views.py
+++ orders/views.py
@@ -2,9 +2,38 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 
 from .forms import AddressForm
+from .models import UserAddress
 
 # Create your views here.
 
 class AddressSelectFormView(FormView):
 	form_class = AddressForm
 	template_name = "orders/address_select.html"
+
+
+	def get_form(self, *args, **kwargs):
+		print "LOG IN Function - get_form in orders.AddressSelectFormView class"
+		form = super(AddressSelectFormView, self).get_form(*args, **kwargs)
+
+		# update queryset ... only shows the address associated with the user
+		print form.fields
+		form.fields["billing_address"].queryset = UserAddress.objects.filter(
+				user__email = self.request.user.email, 
+				type='billing',
+			)
+
+		form.fields["shipping_address"].queryset = UserAddress.objects.filter(
+				user__email = self.request.user.email, 
+				type='shipping',
+			)
+
+		return form 
+
+	def form_valid(self, *args, **kwargs):
+		print "LOG IN Function - form_valid in orders.AddressSelectFormView class"
+		form = super(AddressSelectFormView, self).form_valid(*args, **kwargs)
+		print form
+		return form
+
+	def get_success_url(self, *args, **kwargs):
+		return "/checkout/"
